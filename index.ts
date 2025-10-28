7245fa0a8553d828b5c5f18d319c26f07cd4ec86
Irael <ellycreativity8@gmail.com>
2025-10-28 17:37:50 +0200

    variable and how it is being declared in typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
 console.log("Here is the repo for learning typescript as well as other related course")
+
+
+let a: number = 10;
+let b: string ="here is the maximum of the things you can do with variable and the way you declare them right here.";
