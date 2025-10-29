b6b42cd93c2029662fdb476502f7938a2ed3c581
Irael <ellycreativity8@gmail.com>
2025-10-29 22:21:11 +0200

    here is the progress of what i did in typescrpt learning

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Here is the repo for learning typescript as well as other related course");
+let a = 10;
+let b = "here is the maximum of the things you can do with variable and the way you declare them right here.";
+//# sourceMappingURL=index.js.map
