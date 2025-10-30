de32e9db72f92d4e5b75eb9fe2673ec633a1d88e
Irael <ellycreativity8@gmail.com>
2025-10-30 09:34:46 +0200

    chore: here is the codes about annotation

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,20 @@ console.log("Here is the repo for learning typescript as well as other related c
 
 let a: number = 10;
 let b: string ="here is the maximum of the things you can do with variable and the way you declare them right here.";
+
+
+//  here is all about annotation
+
+
+             //object annotaion
+  let person :{ name:string;age:number;}
+  
+  person = {name:"Elyse",age:30}
+  
+  console.log(person)
+             //function annotation
+function hello(name:string){
+ return name;
+}
+
+console.log(hello("Irael"))
