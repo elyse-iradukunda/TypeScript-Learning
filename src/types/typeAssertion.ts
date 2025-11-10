9ea4868b582590fdafbb3014f2adf72936d78280
Irael <ellycreativity8@gmail.com>
2025-11-10 19:03:24 +0200

    Using <type>

--- src/types/typeAssertion.ts
+++ src/types/typeAssertion.ts
@@ -1,6 +1,12 @@
+// first example using as keyword
 let myName: any = "Elyse";
 let assertion: number = (myName as string).length;
 console.log(assertion);
 
+// here is the above example but used <> backets to do type assertion
 
-// first example using as keyword
+let myName: any = "Elyse";
+let assertion: number = (<string>myName).length;
+console.log(assertion);
+
+
