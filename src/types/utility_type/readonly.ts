2b5c1b45f1e471d62c44b2dc3771ae2db677ac62
Irael <ellycreativity8@gmail.com>
2025-11-15 21:02:33 +0200

    here is about readonly type in typescript

---
+++ src/types/utility_type/readonly.ts
@@ -0,0 +1,19 @@
+ interface Person{
+     
+     name:string;
+     address:string;
+     age:number;
+ }
+
+  type ReadonlyType = Readonly<Person>;
+  
+  let person: ReadonlyType ={
+     name:"elyse",
+     address:"rwand",
+     age:48
+
+ }
+ 
+ person.name ="Ellyse"; // this will throw an erro because we have setted that our object is going tobe read only no thing you can do on it.
+ 
+ 
