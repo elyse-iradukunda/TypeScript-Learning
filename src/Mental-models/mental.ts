39e1ebeb0b40b7f811331ba589f103f5ada7ea91
Irael <ellycreativity8@gmail.com>
2025-11-19 19:01:32 +0200

    Here is the third question

--- src/Mental-models/mental.ts
+++ src/Mental-models/mental.ts
@@ -19,3 +19,19 @@ function describeInput(a:string|number) {
 }
 console.log(describeInput("hi"))
 console.log(describeInput(10))
+
+// third question in mental model session 
+  interface User{
+      name:string;
+      age:number;
+      email:string;
+  }
+let user:User ={
+    name:"Elyse",
+    age:40,
+    email:"Ellycreative@.."
+}
+function introduceUser(user:User) {
+  return `Hello, my name is ${user.name}, I am ${user.age} years old, and my email is ${user.email}`;
+}
+console.log(introduceUser(user))
