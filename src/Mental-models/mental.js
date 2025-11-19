d6c01a0ccfb076a15d6c6bbc7cc815cf316a31ef
Irael <ellycreativity8@gmail.com>
2025-11-19 18:45:10 +0200

    here is the first question on mental model

---
+++ src/Mental-models/mental.js
@@ -0,0 +1,5 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+console.log(addNumbers(2, 3));
+console.log(addNumbers(2.5, 3.5));
---
+++ src/Mental-models/mental.ts
@@ -0,0 +1,7 @@
+
+
+function addNumbers(a:number,b:number):number {
+  return  a+b;
+}
+console.log(addNumbers(2,3));
+console.log(addNumbers(2.5,3.5));
