831b345b03fef45df86d84975464effa249ce12a
Irael <ellycreativity8@gmail.com>
2025-11-22 00:04:42 +0200

    hire is all about second gate question I solved

--- src/types/mappedtype.ts
+++ src/types/mappedtype.ts
@@ -20,3 +20,33 @@ const processRecipe = (recipe: ProcessRecipe) => {
   ],
   instructions: '...',
 });
+
+
+///
+
+
+type direction="up"|"left"|"down"|"up";
+function move(direction: direction, distance: number) {}
+
+// TESTS
+
+move('up', 10);
+move('left', 5);
+
+move(
+  // @ts-expect-error - "up-right" is not a valid direction
+  'up-right',
+  10
+);
+
+move(
+  // @ts-expect-error - "down-left" is not a valid direction
+  'down-left',
+  20
+);
+
+move(
+  'up',
+  // @ts-expect-error - "20" is not a valid distance
+  '20'
+);
