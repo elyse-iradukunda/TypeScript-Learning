fbd83880ca258f698bc052067b40278f2434e38d
Irael <ellycreativity8@gmail.com>
2025-11-21 23:56:02 +0200

    here is all about gate preparation question

--- src/types/mappedtype.ts
+++ src/types/mappedtype.ts
@@ -1,1 +1,22 @@
-typeof
+type ProcessRecipe ={
+    title: string;
+    ingredients:{
+        name:string;
+        quantity:string | number;
+        price?:number;
+    }[]
+    instructions?:string;
+    }
+
+const processRecipe = (recipe: ProcessRecipe) => {
+  console.log(recipe.ingredients);
+};
+
+  processRecipe({
+  title: 'Chocolate Chip Cookies',
+  ingredients: [
+    { name: 'Flour', quantity: 4 },
+    { name: 'Sugar', quantity: '1 cup', price: 5 },
+  ],
+  instructions: '...',
+});
