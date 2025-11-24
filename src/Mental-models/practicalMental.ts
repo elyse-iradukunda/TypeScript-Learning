85f722b8b5bede7820d20135712c4685af1c75aa
Irael <ellycreativity8@gmail.com>
2025-11-24 21:10:44 +0200

    here is today's findings

--- src/Mental-models/practicalMental.ts
+++ src/Mental-models/practicalMental.ts
@@ -10,4 +10,7 @@ let user:User ={name:"elyse",age:30,email:"ely@gmail.com"}
 function introduceUser(user:User) {
   return `Hello, my name is ${user.name}, I am ${user.age} years old, and my email is ${user.email}`;
 }
+
+console.log(introduceUser(user));
+
 console.log(introduceUser(user));
