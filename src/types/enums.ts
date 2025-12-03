e7ca95180bc02e0dbcee8ca21fda22d713ee4005
Irael <ellycreativity8@gmail.com>
2025-12-03 23:25:35 +0200

    here is all about in keyword

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -9,13 +9,30 @@
 
 // console.log(newone)
 
-enum Role{
-  Admin ="Full access",
-  User ="Limited access",
-   Guest ="Read only"
-}
-function getPermissions(role:Role){
-return role
+// enum Role{
+//   Admin ="Full access",
+//   User ="Limited access",
+//    Guest ="Read only"
+// }
+// function getPermissions(role:Role){
+// return role
+// }
+
+// console.log(Role.Guest);
+
+
+function logMessage(){
+  
+enum LogLevel{
+  Info="info",Warning="Warning",Error="Error"
 }
 
-console.log(Role.Guest);
+ if(  "Info" in LogLevel){
+  console.log(true,"");
+ }
+ else{
+  console.log("this")
+ }
+
+}
+logMessage()
