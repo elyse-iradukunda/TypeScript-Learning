9797b0f1727a900a0b2974b5e92e93b67c4dc05b
Irael <ellycreativity8@gmail.com>
2025-12-04 12:46:36 +0200

    5th question

--- src/Mental-models/Gateprep.ts
+++ src/Mental-models/Gateprep.ts
@@ -14,13 +14,22 @@
 
 
 
- type myRange = [Boolean,{name:string,age:number}];
+//  type myRange = [Boolean,{name:string,age:number}];
 
-const setRange = (range: myRange): {}=>{
-  const x = range[0];
-  const y = range[1];
+// const setRange = (range: myRange): {}=>{
+//   const x = range[0];
+//   const y = range[1];
 
-  return { x, y: y.age > 10 };
-};
+//   return { x, y: y.age > 10 };
+// };
 
-console.log(setRange([true, { name: 'Patrick', age: 3}]));
+// console.log(setRange([true, { name: 'Patrick', age: 3}]));
+
+
+
+
+function getFirstElement<T>(arr:T[]):T | undefined{
+     return arr[0]
+   }
+
+console.log(getFirstElement([1, 2, 3]))
